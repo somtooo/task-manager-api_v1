3ceca48a0d8d823da80031b8704c46d61635d566
Somtoo <chukwurahb@gmail.com>
2020-05-25 22:36:58 -0500

    Deployment

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT;
-
+const path = require('path')
+app.get('/', (req,res)=>{
+    res.render('apidoc')
+})
+app.use(express.static(__dirname + "/public"));
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs")
 app.use(express.json());
 app.use(userRouter,taskRouter)
+
 app.listen(port,()=>{
     console.log('Server is up on port ' + port)
 });
--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,8 @@ const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
 const {sendWelcomeEmail,sendCancelationEmail} = require('../emails/account')
+
+
 router.post('/users', async (req,res)=>{
     const user = new User(req.body);
     try {
