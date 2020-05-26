13268daed87aa4646fba440b6a4214cc60bba741
Somtoo <you@example.com>
2020-05-25 22:58:40 -0500

    Deployment

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT;
 const path = require('path')
-app.get('/', (req,res)=>{
-    res.render('apidoc')
-})
+
 app.use(express.static(__dirname + "/public"));
 app.set('views', path.join(__dirname, '/views'));
 app.set("view engine", "ejs")
 app.use(express.json());
 app.use(userRouter,taskRouter)
+
+
+app.get('/', (req,res)=>{
+    res.render('apidoc')
+})
 
 app.listen(port,()=>{
     console.log('Server is up on port ' + port)
